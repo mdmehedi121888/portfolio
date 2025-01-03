5adcd70ad57a0374ea44beb91ae23bf734539371
mehedi <mdmehedi121888@gmail.com>
2025-01-03 23:15:06 +0600

    basic functionality and necessary information are added and rest of the part coming soon

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+export const NavbarData = [
+  { title: "Home", link: "/" },
+  { title: "Services", link: "/services" },
+  { title: "Resume", link: "/resume" },
+  { title: "Work", link: "/work" },
+  { title: "Contact", link: "/contact" },
+];
+
+export const statsData = [
+  { num: 1, text: "Years of experience" },
+  { num: 5, text: "Projects completed" },
+  { num: 8, text: "Technologies mastered" },
+  { num: 500, text: "Code commits" },
+];
+
+export const servicesData = [
+  {
+    _id: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sequi distinctio minus esse consequatur fugit accusamus velit iste quisquam aut.",
+    href: "https://www.github.com/mdmehedi121888",
+  },
+];
